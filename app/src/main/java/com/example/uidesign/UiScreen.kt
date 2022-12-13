8ebf1f922f5e19863c6a1c79fb7be9e209002333
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-13 16:05:12 +0530

    update ui

--- app/src/main/java/com/example/uidesign/UiScreen.kt
+++ app/src/main/java/com/example/uidesign/UiScreen.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class UiScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ui_screen)
+        setContentView(R.layout.sport_mobile_ui)
     }
 }
